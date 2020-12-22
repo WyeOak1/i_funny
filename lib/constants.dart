d13eca14724bb6af1bfaebf5a676bce7f45cd68d
wyeoak <kalchoroev60@gmail.com>
2020-12-22 17:54:15 +0600

    added constants

---
+++ lib/constants.dart
@@ -0,0 +1,34 @@
+String featuredUrl = 'https://api.ifunny.mobi/v4/feeds/featured';
+Map<String, String> featuredRequestHeaders = {
+  'Accept': 'application/json,image/jpeg,image/webp,video/mp4',
+  'iFunny-Project-Id': 'Russia',
+  'Accept-Language': 'ru-RU',
+  'Messaging-Project': 'idaprikol.ru:idaprikol-60aec',
+  'ApplicationState': '1',
+  'Authorization':
+      'Basic MDIzNTYxMzY2NDYxMzY2MzMzMkQ2NDMyMzU2MzJEMzQ2MzY0NjQyRDM4Mzg2MzM1MkQzMDMyMzQzNjYyMzkzOTMxNjEzNjY2MzZfUHpuNEQxMnNvSzo5Nzg0ZjE2MzZlYzdhYjE4YmI5YzczNmNhZjg0MzY1Mzc3M2M5Y2Mz',
+  'Host': 'api.ifunny.mobi',
+  'Connection': 'close',
+  'Accept-Encoding': 'gzip, deflate',
+  'If-Modified-Since': 'Tue, 08 Dec 2020 19:55:50 GMT',
+  'User-Agent':
+      'iDaPrikol/6.15.4(1119163) Android/6.0 (Xiaomi; Redmi Note 4; Xiaomi)',
+};
+
+
+String channelsUrl = 'https://api.ifunny.mobi/v4/channels';
+Map<String, String> channelsRequestHeaders = {
+  'Accept': 'application/json,image/jpeg,image/webp,video/mp4',
+  'iFunny-Project-Id': 'Russia',
+  'Accept-Language': 'ru-RU',
+  'Messaging-Project': 'idaprikol.ru:idaprikol-60aec',
+  'ApplicationState': '1',
+  'Authorization':
+      'Basic MDIzNTYxMzY2NDYxMzY2MzMzMkQ2NDMyMzU2MzJEMzQ2MzY0NjQyRDM4Mzg2MzM1MkQzMDMyMzQzNjYyMzkzOTMxNjEzNjY2MzZfUHpuNEQxMnNvSzo5Nzg0ZjE2MzZlYzdhYjE4YmI5YzczNmNhZjg0MzY1Mzc3M2M5Y2Mz',
+  'Host': 'api.ifunny.mobi',
+  'Connection': 'close',
+  'Accept-Encoding': 'gzip, deflate',
+  'If-Modified-Since': 'Tue, 08 Dec 2020 19:55:50 GMT',
+  'User-Agent':
+      'iDaPrikol/6.15.4(1119163) Android/6.0 (Xiaomi; Redmi Note 4; Xiaomi)',
+};
